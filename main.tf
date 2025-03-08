cdb0c2a65795de2ded41af4bb84d42a1907a8bad
nova34tkj4 <novahariyakusuma@gmail.com>
2025-03-08 11:09:38 +0700

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,10 @@
+provider "google" {
+  credentials = file("/home/dana/terraform-digital-skola/secret.json")
+  project = "ftde-batch-4"
+  region  = "us-central1"  # Replace with the desired GCP region
+}
+
+resource "google_storage_bucket" "my_bucket" {
+  name     = "digital-skolah-bucket-test"  # Replace with your unique bucket name
+  location = "US"  # Specify the bucket's location (e.g., "US" or "EU")
+}
